ef4adbd6e68db7927104d733d3a3defe2d8de42b
Rinor Rama <rinor.rama95@gmail.com>
2021-11-17 20:37:12 +0100

    some code refactoring

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
+let message;
 for (let i = 1; i <= 840; i++) {
-	if (i % (3 * 5 * 7 * 8) === 0) {
-		console.log("FizzBuzzFishBus");
-	} else if (i % (5 * 7 * 8) === 0) {
-		console.log("BuzzFishBus");
-	} else if (i % (3 * 7 * 8) === 0) {
-		console.log("FizzFishBus");
-	} else if (i % (3 * 5 * 8) === 0) {
-		console.log("FizzBuzzBus" === 0);
-	} else if (i % (3 * 5 * 7) === 0) {
-		console.log("FizzBuzzFish");
-	} else if (i % (7 * 8) === 0) {
-		console.log("FishBus");
-	} else if (i % (5 * 8) === 0) {
-		console.log("BuzzBus");
-	} else if (i % (3 * 8) === 0) {
-		console.log("FizzBus");
-	} else if (i % (5 * 7) === 0) {
-		console.log("BuzzFish");
-	} else if (i % (3 * 7) === 0) {
-		console.log("FizzFish");
-	} else if (i % (3 * 5) === 0) {
-		console.log("FizzBuzz");
-	} else if (i % 8 === 0) {
-		console.log("Bus");
-	} else if (i % 7 === 0) {
-		console.log("Fish");
-	} else if (i % 5 === 0) {
-		console.log("Buzz");
-	} else if (i % 3 === 0) {
-		console.log("Fizz");
-	} else {
+	message = "";
+	if (i % 3 === 0) {
+		message = message + "Fizz";
+	}
+	if (i % 5 === 0) {
+		message = message + "Buzz";
+	}
+	if (i % 7 === 0) {
+		message = message + "Fish";
+	}
+	if (i % 8 === 0) {
+		message = message + "Bus";
+	}
+	if (message === "") {
 		console.log(i);
+	} else {
+		console.log(message);
 	}
 }
 console.log("The first number that results in FizzBuzzFishBus is", 3 * 5 * 7 * 8);
